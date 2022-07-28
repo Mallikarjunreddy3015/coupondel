e9292c49727ce7babbf3f8f5005c4a83e57bfab3
Mallikarjuna reddy <reddyvarimallikarjunreddy@gmail.com>
2022-07-28 23:00:47 +0530

    change

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,6 @@ const express = require("express");
 const app=express();
 const path=require("path");
 
-
-
-
-
 if (process.env.NODE_ENV !== "PRODUCTION") {
     require("dotenv").config({ path: "backend/config/config.env" });
   }
