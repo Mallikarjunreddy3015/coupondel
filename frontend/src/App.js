e38756ee3cbdc372ceeb1877d67bfccfa4c4be0e
Subhadip <mnhacker2001@gmail.com>
2022-08-01 23:48:12 +0530

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import React from "react";
+import Header from "./components/Header/Header";
+import About from "./components/About/About";
+
 import logo from "./Images/me.png";
 
 function App() {
   return (
     <div>
-      <h1>Welcome you to Coupondel</h1>
-
-      <img src={logo} alt="logo" />
+      <h1>hello</h1>
+      <Header />
+      <About />
     </div>
   );
 }
--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -1,1 +1,7 @@
 import React from "react";
+
+function About() {
+  return <h2>This is About Section</h2>;
+}
+
+export default About;
