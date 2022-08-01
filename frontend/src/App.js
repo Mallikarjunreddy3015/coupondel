2c23a4c9c43a14dff0852f5fb3eff52a6dfb91b0
Subhadip <mnhacker2001@gmail.com>
2022-08-01 19:28:25 +0530

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,14 @@
 import "./App.css";
 import React from "react";
-import logo from './Images/me.png';
+import logo from "./Images/me.png";
 
+function App() {
+  return (
+    <div>
+      <h1>Welcome you to Coupondel</h1>
 
-
-function App(){
-
-return(
-<div>
-<h1>
-    Welcome to Coupondel
-</h1>
-
-<img src={ logo} alt="logo" />
-</div>
-
-)
-
-
+      <img src={logo} alt="logo" />
+    </div>
+  );
 }
 export default App;
