e7c8551a221103764c52bd744077cd3aab97cff3
deepak <bairwaddeepak@gmail.com>
2022-08-03 20:07:41 +0530

    updating footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -7,44 +7,48 @@ const Footer = () => {
     <>
       <div className="footer-container">
         <div className="footer-main">
+        <img src={icon} alt="icon" />
           <div className="footer-links">
-            <img src={icon} alt="icon" />
-            <div className="social-Media">
-              <li>
-                <a href="#"></a>
-              </li>
-              <li>
-                <a href="#"></a>
-              </li>
-              <li>
-                <a href="#"></a>
-              </li>
+            
+            <div className="social-Media footer-col">
+              <li> <a href="#"><i class="fi fi-brands-linkedin"></i></a></li>
+              <li><a href="#"><i class="fi fi-brands-instagram"></i></a></li>
+              <li><a href="#"><i class="fi fi-brands-facebook"></i></a></li>
+              <li><a href="#"><i class="fi fi-brands-twitter"></i></a></li>
             </div>
+            <div className="footer-col">
             <ul>
-              <li>About</li>
-              <li>Partners</li>
-              <li>Team</li>
+              <li><a href="#">about us</a></li>
+              <li><a href="#">Partners</a></li>
+              <li><a href="#">Team</a></li>
             </ul>
+            </div>
+            <div className="footer-col">
             <ul>
-              <li>Contact</li>
-              <li>Partners</li>
-              <li>Team</li>
+              <li><a href="#">Contact</a></li>
+              <li><a href="#">email</a></li>
+              <li><a href="#">phone</a></li>
             </ul>
+            </div>
+            <div className="footer-col">
             <ul>
-              <li>Policy</li>
-              <li>Partners</li>
-              <li>Team</li>
+              <li><a href="#">Policy</a></li>
+              <li><a href="#">How to use</a></li>
+              <li><a href="#">some more</a></li>
             </ul>
+            </div>
+            <div className="footer-col">
             <ul>
-              <li>More</li>
-              <li>Partners</li>
-              <li>Team</li>
+              <li>importent links</li>
+              <li><a href="#">explore amazone</a></li>
+              <li><a href="#">extra discount</a></li>
             </ul>
+            </div>
           </div>
         </div>
       </div>
       <div className="footer-copy">
-        <p>&copy; CouponDel {year}</p>
+        <p>&copy;CouponDel {year} All rights Reserved</p>
       </div>
     </>
   );
