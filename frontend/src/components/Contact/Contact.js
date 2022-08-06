54ce18676087ecf52b7ba8561aceffbb1b7edb52
Mallikarjuna reddy <reddyvarimallikarjunreddy@gmail.com>
2022-08-07 00:47:03 +0530

    .

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -1,27 +1,41 @@
 import React, { useState, useRef } from "react";
+import Backdrop from '@material-ui/core/Backdrop';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import "./Contact.css";
 import Button from "../Button";
 import swal from 'sweetalert';
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+}));
 
 const Contact = () => {
-
+  const [loading, setLoading] = useState("1");
+  const classes=useStyles();
   const formRef = useRef(null)
   const scriptUrl = "https://script.google.com/macros/s/AKfycbzWO5BhvQCIl3BKBme2IgxfdQhupCrFeOi_WOl7ProkdA7wHbtlS8pNO7j7TFlcL0g-FA/exec"
   const handleSubmit = (e) => {
-    e.preventDefault()
-    
+    e.preventDefault();
+  
 
     fetch(scriptUrl, {
       method: 'POST',
       body: new FormData(formRef.current),
-
-    }).then(res => {
-
+      
+    }).then(
+      setLoading("3")
+    )
+    
+    .then(res => {
+setLoading("2");
       swal(`Thank you ${e.target.name.value}`, "We will contact you soon!")
-        .then(()=> {
+        .then(() => {
           window.location.reload()
         });
-        
+
     })
       .catch(err => console.log(err))
   }
@@ -54,6 +68,11 @@ const Contact = () => {
             required
           ></textarea>
           <Button name={"Submit"} type="submit" />
+
+          {loading =="3" ?  <Backdrop className={classes.backdrop} open>
+            <CircularProgress color="inherit" />
+          </Backdrop>:loading}
+
         </form>
       </div>
     </>
