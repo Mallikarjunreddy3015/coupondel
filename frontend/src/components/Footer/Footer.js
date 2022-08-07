7d347c46ddc895facd8d80a9b8ca0ed9974fd3e7
Mallikarjuna reddy <reddyvarimallikarjunreddy@gmail.com>
2022-08-07 17:47:33 +0530

    .

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -28,7 +28,7 @@ const Footer = () => {
                   </a>
                 </li>
                 <li>
-                  <a href="#">
+                  <a href="https://twitter.com/coupondel_">
                     <i className="fi fi-brands-twitter"></i>
                   </a>
                 </li>
