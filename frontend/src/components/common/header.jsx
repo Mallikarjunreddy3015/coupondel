a140c5af407acf57dffd42ecef74289393907b90
Subhadip <mnhacker2001@gmail.com>
2022-08-14 00:35:42 +0530

    ...

--- frontend/src/components/common/header.jsx
+++ frontend/src/components/common/header.jsx
@@ -4,8 +4,14 @@ import SearchIcon from '@mui/icons-material/Search';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import HelpIcon from '@mui/icons-material/Help';
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import MenuIcon from '@mui/icons-material/Menu';
 import { Container } from "@mui/system";
+import styled from "@emotion/styled";
 
+
+const StyledTab = styled(Tab)({
+    textTransform : "none" 
+})
 
 const Header = () => {
     return(
@@ -19,15 +25,15 @@ const Header = () => {
             padding : "10px 0"
             }}>
           
-          <img width={200}  src="http://www.coupondel.com/imgs/logo-icon.ee909b75407f5be0b7f5bd5a84693622.png" alt="#" />
+          <img style={{margin : "0 0 0 20px"}} width={200}  src="http://www.coupondel.com/imgs/logo-icon.ee909b75407f5be0b7f5bd5a84693622.png" alt="#" />
 
-            <Tabs sx={{width : "auto" , margin : "auto" , backgroundColor : "dodgerblue" ,}}>
-                <Tab sx={{textTransform : "none" , mr : 4}} label="Stores" />
-                <Tab sx={{textTransform : "none" , mr : 4}} label="Coupons" />
-                <Tab  label="Refer & Earn" />
+            <Tabs sx={{width : "auto" , margin : "auto" ,display : {xs : "none" , sm : "block"}}}>
+                <StyledTab sx={{mr : 4}} label="Stores" />
+                <StyledTab sx={{mr : 4}} label="Coupons" />
+                <StyledTab label="Refer & Earn" />
             </Tabs>
 
-            <Box sx={{ mr : 4}}>
+            <Box  sx={{mr : 1 , display : {sm : "none" , md : "block" , xs : "none"}}}>
                 <IconButton sx={{mr : 2}}>
                     <SearchIcon />
                 </IconButton>
@@ -41,10 +47,11 @@ const Header = () => {
                     <AccountCircleIcon />
                 </IconButton>
 
-                <IconButton sx={{mr : 2}}>
-                    <HelpIcon />
-                </IconButton>
+                
             </Box>
+            <IconButton sx={{ml : "auto", mr : 3 , display : {xs : "block" ,  md : "none"}}}>
+                    <MenuIcon />
+            </IconButton>
             
             
            
