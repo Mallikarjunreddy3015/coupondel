a1154984f3fb45a90ae94deced19ad119e1f159a
Subhadip <mnhacker2001@gmail.com>
2022-08-13 23:30:59 +0530

    ..

--- frontend/src/components/common/header.jsx
+++ frontend/src/components/common/header.jsx
@@ -9,15 +9,15 @@ import { Container } from "@mui/system";
 const Header = () => {
     return(
         <>
-          <AppBar className="AppBar" sx={{boxShadow : "none" , display : "flex"  }}>
+          <AppBar className="AppBar" sx={{backgroundColor : "#fff", display : "flex"  }}>
           <Container maxWidth = "xl">
-          <img width={200} height={"auto"} src="http://www.coupondel.com/imgs/footer-logo-icon-white.f4a976bc89294d2affe5402dadfd5734.png" alt="#" />
+          <img  width={200}  src="http://www.coupondel.com/imgs/logo-icon.ee909b75407f5be0b7f5bd5a84693622.png" alt="#" />
             <Toolbar className="Toolbar">
 
                 <Tabs sx={{ width : "70%" ,m : "auto" }}>
-                    <Tab sx={{color : "#fff"}} label="Stores" />
-                    <Tab sx={{color : "#fff"}} label="Coupons" />
-                    <Tab sx={{color : "#fff"}} label="Refer & Earn" />
+                    <Tab label="Stores" />
+                    <Tab label="Coupons" />
+                    <Tab label="Refer & Earn" />
                 </Tabs>
 
             </Toolbar>
