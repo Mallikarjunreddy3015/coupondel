3cd48492defe4e62fc4924e784d9d6ac7a8fb853
deepak <bairwaddeepak@gmail.com>
2022-08-20 22:04:20 +0530

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,15 @@ import Affiliates from "./components/affiliates/affiliates.jsx";
 import Header from "./components/common/Header.jsx";
 import FootNav from "./components/common/FootNav.jsx";
 import Home from "./components/home/home.js";
+import { Routes,Route, NavLink } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <Home />
-      <Affiliates />
+    <><Header />
+     <Routes> 
+      <Route path="/" element={<Home/>}/>
+      <Route path="/affiliates" element={<Affiliates />}/>
+     </Routes>
       <FootNav />
     </>
   );
