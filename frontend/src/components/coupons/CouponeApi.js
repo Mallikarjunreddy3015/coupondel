77c8b30c44337a0be63b1a882e9be15f0632302f
deepak <bairwaddeepak@gmail.com>
2022-08-25 20:00:57 +0530

    .

---
+++ frontend/src/components/coupons/CouponeApi.js
@@ -0,0 +1,74 @@
+import Zomato from "../../images/zomato.png"
+import Swiggy from "../../images/swiggy.png"
+const CouponeApi =[
+    {
+        id:1,
+        img:`${Zomato}`,
+        brandName:"",
+        heading:"Get flat 15% off on your next order",   
+        tc:""
+    },
+    {
+        id:2,
+        img:`${Swiggy}`,
+        brandName:"",
+        heading:"Get flat 15% off on your next order",
+        discount:30,    
+        tc:""
+    },
+    {
+        id:3,
+        img:`${Zomato}`,
+        brandName:"",
+        heading:"Get flat 15% off on your next order",
+        discount:30,    
+        tc:""
+    },
+    {
+        id:1,
+        img:`${Zomato}`,
+        brandName:"",
+        heading:"Get flat 15% off on your next order",   
+        tc:""
+    },
+    {
+        id:2,
+        img:`${Swiggy}`,
+        brandName:"",
+        heading:"Get flat 15% off on your next order",
+        discount:30,    
+        tc:""
+    },
+    {
+        id:3,
+        img:`${Zomato}`,
+        brandName:"",
+        heading:"Get flat 15% off on your next order",
+        discount:30,    
+        tc:""
+    },
+    {
+        id:1,
+        img:`${Zomato}`,
+        brandName:"",
+        heading:"Get flat 15% off on your next order",   
+        tc:""
+    },
+    {
+        id:2,
+        img:`${Swiggy}`,
+        brandName:"",
+        heading:"Get flat 15% off on your next order",
+        discount:30,    
+        tc:""
+    },
+    {
+        id:3,
+        img:`${Zomato}`,
+        brandName:"",
+        heading:"Get flat 15% off on your next order",
+        discount:30,    
+        tc:""
+    },
+]
+export {CouponeApi}
