04b2f29e6632fb11a86f6106c3c4db642f820b2c
Mallikarjuna reddy <reddyvarimallikarjunreddy@gmail.com>
2022-08-26 09:36:22 -0700

    Delete backend directory

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-//const connectDatabase = require("./config/database");
-
-
-
-app.listen(process.env.PORT, () => {
-    console.log(`Listening  on http://localhost:${process.env.PORT}`);
-});
