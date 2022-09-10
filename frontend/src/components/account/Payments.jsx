bbed4202eceed950336519ab38278699920d7faa
Subhadip <mnhacker2001@gmail.com>
2022-09-11 00:38:14 +0530

    .

--- frontend/src/components/account/Payments.jsx
+++ frontend/src/components/account/Payments.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
 
 const Payments = () => {
@@ -25,8 +25,94 @@ const Payments = () => {
           </h3>
           <p>Minimum Withdraw : 50 rupees</p>
         </Box>
-        <Box>
-          <h4>Choose a Withdraw Method</h4>
+        <Box
+          sx={{
+            mt: 2,
+            mb: 1,
+          }}
+          component="form"
+        >
+          <h4 style={{ margin: "0 0 10px 0" }}>Choose a Withdraw Method</h4>
+
+          <Box sx={{ mb: 2 }}>
+            <p>1. Using UPI</p>
+            <TextField
+              size="small"
+              variant="filled"
+              label="Enter your upi id"
+            />
+            <Button
+              sx={{
+                backgroundColor: "green",
+                fontSize: "0.6rem",
+                color: "#fff",
+                "&:hover": {
+                  backgroundColor: "green",
+                },
+              }}
+            >
+              Verify
+            </Button>
+          </Box>
+          <Box>
+            <p>2. Using paytem wallet</p>
+            <TextField
+              size="small"
+              variant="standard"
+              label="Enter your paytm no"
+            />
+            <Button
+              sx={{
+                backgroundColor: "green",
+                fontSize: "0.6rem",
+                color: "#fff",
+                "&:hover": {
+                  backgroundColor: "green",
+                },
+              }}
+            >
+              Verify
+            </Button>
+          </Box>
+
+          <Box>
+            <h4>Enter Amount</h4>
+            <TextField
+              variant="standard"
+              label="Minimun amount is 50rs"
+              size="small"
+            />
+          </Box>
+          <Button
+            type="submit"
+            variant="outlined"
+            onClick={(e) => {
+              e.preventDefault();
+              alert("Withdraw request submitted");
+            }}
+          >
+            Submit
+          </Button>
+        </Box>
+
+        <Box
+          sx={{
+            mt: 2,
+          }}
+        >
+          <h2>All Withdrawals</h2>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              backgroundColor: "orange",
+            }}
+          >
+            <h4>70 Rupees</h4>
+            <h5>Status : Success</h5>
+          </Box>
         </Box>
       </Box>
     </>
